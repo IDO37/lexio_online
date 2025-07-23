88750aa671df4c050ca7743074b8cbbdb2ed9220
IDO37 <charles5275407@gmail.com>
2025-07-23 15:13:59 +0900

    up

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,14 @@
 <script setup>
-import HelloWorld from './components/HelloWorld.vue'
+import Navbar from './components/Navbar.vue'
 </script>
 
 <template>
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
+  <div class="min-h-screen bg-gray-900 text-white">
+    <Navbar />
+    <main class="pt-20">
+      <router-view />
+    </main>
   </div>
-  <HelloWorld msg="Vite + Vue" />
 </template>
 
 <style scoped>
